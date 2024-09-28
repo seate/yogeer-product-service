bc8f7fd4e599f09ac2f260f99143186b509c6055
haebyun <bhk4392@gmail.com>
2024-09-28 22:45:27 +0900

    init - 상품 서비스 관련 초기 세팅
    - java 21
    - springBoot 3.3.4
    - web, validation, lombok 의존성 추가

---
+++ src/main/java/com/yoger/productserviceorganization/ProductServiceOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.yoger.productserviceorganization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceOrganizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceOrganizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoger/productserviceorganization/ProductServiceOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoger.productserviceorganization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceOrganizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
