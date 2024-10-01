8b2cb21ce0b22a1e768a1f78e1526270bddc8c99
haebyun <bhk4392@gmail.com>
2024-10-01 21:05:10 +0900

    refactor : SellableProductResponseDTO 리팩토링
    - of 메서드를 from 메서드로 변경하여 ProductEntity를 받아 변환하는 방식으로 수정

--- src/main/java/com/yoger/productserviceorganization/proruct/dto/response/SellableProductResponseDTO.java
+++ src/main/java/com/yoger/productserviceorganization/proruct/dto/response/SellableProductResponseDTO.java
@@ -1,6 +1,7 @@
 package com.yoger.productserviceorganization.proruct.dto.response;
 
 import com.yoger.productserviceorganization.proruct.domain.ProductState;
+import com.yoger.productserviceorganization.proruct.persistence.ProductEntity;
 
 public record SellableProductResponseDTO(
         String name,
@@ -9,8 +10,13 @@ public record SellableProductResponseDTO(
         String imageUrl,
         ProductState state
 ) {
-    public static SellableProductResponseDTO of(String name, String priceByQuantity, String description, String imageUrl,
-                                                ProductState state) {
-        return new SellableProductResponseDTO(name, priceByQuantity, description, imageUrl, state);
+    public static SellableProductResponseDTO from(ProductEntity productEntity) {
+        return new SellableProductResponseDTO(
+                productEntity.getName(),
+                productEntity.getPriceByQuantity(),
+                productEntity.getDescription(),
+                productEntity.getImageUrl(),
+                productEntity.getState()
+        );
     }
 }
