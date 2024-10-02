26950d5e6551af8adb42e63f423a0152f7c2c142
haebyun <bhk4392@gmail.com>
2024-10-02 17:20:40 +0900

    feat : 커스텀 이미지 파일 유효성 검사 어노테이션 추가
    - ValidImage 어노테이션을 추가하여 이미지 파일의 MIME 타입 검증 기능 구현
    - ImageFileValidator 클래스를 작성하여 jpeg, png, gif 형식의 파일만 허용하는 유효성 검사 로직 추가

---
+++ src/main/java/com/yoger/productserviceorganization/proruct/validation/ImageFileValidator.java
@@ -0,0 +1,19 @@
+package com.yoger.productserviceorganization.proruct.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageFileValidator implements ConstraintValidator<ValidImage, MultipartFile> {
+    @Override
+    public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
+        // MIME 타입 확인
+        String contentType = file.getContentType();
+        if (contentType == null) {
+            return false;
+        }
+        return contentType.equals("image/jpeg") ||
+                contentType.equals("image/png") ||
+                contentType.equals("image/gif");
+    }
+}
