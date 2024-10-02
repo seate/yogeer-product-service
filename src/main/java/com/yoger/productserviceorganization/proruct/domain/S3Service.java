e9bca5bf9fe7757e6d4d1c95644f35a62aba194d
haebyun <bhk4392@gmail.com>
2024-10-02 17:38:42 +0900

    feat : S3 이미지 업로드 서비스 구현
    - S3Client를 사용하여 이미지를 S3 버킷에 업로드하는 메서드 추가
    - 업로드 후 이미지의 S3 URL 반환
    - S3 업로드 및 파일 처리 중 발생할 수 있는 예외 처리

---
+++ src/main/java/com/yoger/productserviceorganization/proruct/domain/S3Service.java
@@ -0,0 +1,41 @@
+package com.yoger.productserviceorganization.proruct.domain;
+
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String uploadImage(MultipartFile image) {
+        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .contentType(image.getContentType())
+                    .contentLength(image.getSize())
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(image.getInputStream(), image.getSize()));
+
+            return String.format("https://%s.s3.ap-northeast-2.amazonaws.com/%s", bucketName, fileName);
+        } catch (S3Exception e) {
+            throw new RuntimeException("S3에 파일 업로드 중 오류 발생: " + e.getMessage(), e);
+        } catch (IOException e) {
+            throw new RuntimeException("파일 처리 중 오류 발생: " + e.getMessage(), e);
+        }
+    }
+}
