96a36748630d3784c2785ec46de17aa7e6856da9
haebyun <bhk4392@gmail.com>
2024-10-02 17:40:49 +0900

    feat : DemoProduct 저장을 위한 REST API 엔드포인트 추가
    - "/api/products" 경로에 POST 요청을 처리하는 메서드 추가
    - 유효성 검사를 위한 @Valid 어노테이션 사용
    - DemoProductRequestDTO를 받아 ProductService의 saveDemoProduct 메서드 호출

--- src/main/java/com/yoger/productserviceorganization/proruct/controller/ProductController.java
+++ src/main/java/com/yoger/productserviceorganization/proruct/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.yoger.productserviceorganization.proruct.controller;
 
 import com.yoger.productserviceorganization.proruct.domain.ProductService;
+import com.yoger.productserviceorganization.proruct.dto.request.DemoProductRequestDTO;
+import com.yoger.productserviceorganization.proruct.dto.response.DemoProductResponseDTO;
 import com.yoger.productserviceorganization.proruct.dto.response.SellableProductResponseDTO;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,10 @@ public class ProductController {
     @GetMapping
     public List<SellableProductResponseDTO> getSellableProducts() {
         return productService.viewSellableProducts();
+    }
+
+    @PostMapping
+    public DemoProductResponseDTO saveDemo(@Valid @ModelAttribute DemoProductRequestDTO demoProductRequestDTO) {
+        return productService.saveDemoProduct(demoProductRequestDTO);
     }
 }
