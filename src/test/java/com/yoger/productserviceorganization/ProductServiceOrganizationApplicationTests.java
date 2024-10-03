ad7b5e507ca35075cb85f3991efab8819ec100c7
haebyun <bhk4392@gmail.com>
2024-10-03 21:50:08 +0900

    test : 제품 생성 API 통합 테스트 추가
    - S3에 이미지 업로드 후 제품이 정상적으로 생성되는지 확인하는 통합 테스트 추가
    - `WebTestClient`를 사용하여 제품 생성 API 호출 및 이미지 업로드 검증
    - 응답 본문에서 제품 정보 및 이미지 URL 패턴 검증

--- src/test/java/com/yoger/productserviceorganization/ProductServiceOrganizationApplicationTests.java
+++ src/test/java/com/yoger/productserviceorganization/ProductServiceOrganizationApplicationTests.java
@@ -1,15 +1,78 @@
 package com.yoger.productserviceorganization;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import com.yoger.productserviceorganization.proruct.domain.ProductState;
+import com.yoger.productserviceorganization.proruct.domain.S3Service;
+import com.yoger.productserviceorganization.proruct.dto.response.DemoProductResponseDTO;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = LocalStackS3Config.class
+)
 @ActiveProfiles("integration")
+@Testcontainers
 class ProductServiceOrganizationApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    private S3Service s3Service; // S3Service를 주입
+
+    @Autowired
+    private S3Client s3TestClient;
+
+    String bucketName = "test-integration-bucket";
+
+    @BeforeEach
+    public void setUp() {
+        // ReflectionTestUtils를 사용해 bucketName 강제 주입
+        ReflectionTestUtils.setField(s3Service, "bucketName", bucketName);
+
+        // 버킷을 미리 생성해 둡니다.
+        s3TestClient.createBucket(CreateBucketRequest.builder().bucket(bucketName).build());
     }
 
+    @Test
+    void whenPostRequestThenProductCreated() throws IOException {
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("name", "Test Product");
+        builder.part("description", "This is a test product description");
+        builder.part("image", new ClassPathResource("test-image.jpeg"))
+                .filename("test-image.jpeg")
+                .contentType(MediaType.IMAGE_JPEG);
+
+        DemoProductResponseDTO demoProductResponseDTO = webTestClient.post()
+                .uri("/api/products")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(DemoProductResponseDTO.class)  // DTO로 응답 본문을 매핑
+                .returnResult()
+                .getResponseBody();
+
+        String expectedImageUrlPattern = "https://test-integration-bucket\\.s3\\.ap-northeast-2\\.amazonaws\\.com/[a-f0-9\\-]+_test-image\\.jpeg";
+
+        assertThat(demoProductResponseDTO.name()).isEqualTo("Test Product");
+        assertThat(demoProductResponseDTO.description()).isEqualTo("This is a test product description");
+        assertThat(demoProductResponseDTO.imageUrl()).matches(expectedImageUrlPattern);
+        assertThat(demoProductResponseDTO.state()).isEqualTo(ProductState.DEMO);
+    }
 }
