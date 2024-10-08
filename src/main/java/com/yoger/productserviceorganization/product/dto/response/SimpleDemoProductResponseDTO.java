f1d5cb2b59099dd728995f6f6324c6fb3f81b643
haebyun <bhk4392@gmail.com>
2024-10-09 00:32:56 +0900

    feat: SimpleDemoProductResponseDTO와 SimpleSellableProductResponseDTO에 from 메서드 추가
    - SimpleDemoProductResponseDTO에 ProductEntity에서 DTO로 변환하는 from 메서드 추가
    - SimpleSellableProductResponseDTO에 ProductEntity에서 DTO로 변환하는 from 메서드 추가
    - SimpleSellableProductResponseDTO에서는 initialStockQuantity와 stockQuantity를 통해 soldQuantity 계산

--- src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleDemoProductResponseDTO.java
+++ src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleDemoProductResponseDTO.java
@@ -2,6 +2,7 @@ package com.yoger.productserviceorganization.product.dto.response;
 
 import com.yoger.productserviceorganization.product.domain.model.PriceByQuantity;
 import com.yoger.productserviceorganization.product.domain.model.ProductState;
+import com.yoger.productserviceorganization.product.persistence.ProductEntity;
 import java.util.List;
 
 public record SimpleDemoProductResponseDTO(
@@ -12,4 +13,14 @@ public record SimpleDemoProductResponseDTO(
         String creatorName,
         ProductState state
 ) {
+    public static SimpleDemoProductResponseDTO from(ProductEntity productEntity) {
+        return new SimpleDemoProductResponseDTO(
+                productEntity.getId(),
+                productEntity.getName(),
+                productEntity.getPriceByQuantities(),
+                productEntity.getThumbnailImageUrl(),
+                productEntity.getCreatorName(),
+                productEntity.getState()
+        );
+    }
 }
--- src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleSellableProductResponseDTO.java
+++ src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleSellableProductResponseDTO.java
@@ -2,6 +2,7 @@ package com.yoger.productserviceorganization.product.dto.response;
 
 import com.yoger.productserviceorganization.product.domain.model.PriceByQuantity;
 import com.yoger.productserviceorganization.product.domain.model.ProductState;
+import com.yoger.productserviceorganization.product.persistence.ProductEntity;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,8 +12,21 @@ public record SimpleSellableProductResponseDTO(
         List<PriceByQuantity> priceByQuantities,
         String thumbnailImageUrl,
         String creatorName,
-        int soldAmount,
+        int soldQuantity,
         LocalDateTime dueDate,
         ProductState state
 ) {
+    public static SimpleSellableProductResponseDTO from(ProductEntity productEntity) {
+        int soldQuantity = productEntity.getInitialStockQuantity() - productEntity.getStockQuantity(); // 판매된 수량 계산
+        return new SimpleSellableProductResponseDTO(
+                productEntity.getId(),
+                productEntity.getName(),
+                productEntity.getPriceByQuantities(),
+                productEntity.getThumbnailImageUrl(),
+                productEntity.getCreatorName(),
+                soldQuantity,
+                productEntity.getDueDate(),
+                productEntity.getState()
+        );
+    }
 }
