62a92d5dfd0225a8d06d9612f48ed8384503dc02
haebyun <bhk4392@gmail.com>
2024-10-09 00:13:54 +0900

    feat: ProductEntity에 추가 필드 추가
    - thumbnailImageUrl, creatorId, creatorName, dueDate, initialStockQuantity, stockQuantity 필드를 ProductEntity에 추가
    - 유효성 검사를 위해 각 필드에 NotBlank, NotNull 어노테이션 추가
    - 상품 생성 시 새로운 필드를 반영한 of 메서드 수정

--- src/main/java/com/yoger/productserviceorganization/product/persistence/ProductEntity.java
+++ src/main/java/com/yoger/productserviceorganization/product/persistence/ProductEntity.java
@@ -53,7 +53,7 @@ public class ProductEntity {
     @Size(min = 10, max = 500, message = "상품 상세 설명은 10글자 이상 500글자 이하만 가능합니다.")
     private String description;
 
-    @NotNull(message = "상품에 대한 사진을 추가해주세요.")
+    @NotBlank(message = "상품에 대한 사진을 추가해주세요.")
     @Pattern(
             regexp = "^(https?)://[a-zA-Z0-9\\-]+\\.s3\\.[a-zA-Z0-9\\-]+\\.amazonaws\\.com/.*$",
             message = "유효한 S3 URL 형식이어야 합니다."
@@ -64,19 +64,54 @@ public class ProductEntity {
     @Enumerated(EnumType.STRING)
     private ProductState state;
 
+    @NotBlank(message = "상품에 대한 대표 사진을 추가해주세요.")
+    @Pattern(
+            regexp = "^(https?)://[a-zA-Z0-9\\-]+\\.s3\\.[a-zA-Z0-9\\-]+\\.amazonaws\\.com/.*$",
+            message = "유효한 S3 URL 형식이어야 합니다."
+    )
+    private String thumbnailImageUrl;
+
+    @NotNull
+    private Long creatorId;
+
+    @NotBlank(message = "제작자의 이름을 입력해주세요.")
+    private String creatorName;
+
     @CreatedDate
     private LocalDateTime createdDate;
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
+    @Nullable
+    private LocalDateTime dueDate;
+
+    private int initialStockQuantity;
+
+    private int stockQuantity;
+
     /*
      * @Version을 통한 낙관적 락킹을 할 수 있지만,
      * Product의 경우에는 다수의 사용자가 동일한 상품을 수정하는 경우는
      * 존재하지 않기 때문에 작성하지 않음
      */
-    public static ProductEntity of(String name, List<PriceByQuantity> priceByQuantities, String description, String imageUrl,
-                                   ProductState state) {
-        return new ProductEntity(null, name, priceByQuantities, description, imageUrl, state, null, null);
+    public static ProductEntity of(String name, List<PriceByQuantity> priceByQuantities, String description, String imageUrl, ProductState state,
+                                    String thumbnailImageUrl, Long creatorId, String creatorName ,LocalDateTime dueDate, int initialStockQuantity, int stockQuantity) {
+        return new ProductEntity(
+                null,
+                name,
+                priceByQuantities,
+                description,
+                imageUrl,
+                state,
+                thumbnailImageUrl,
+                creatorId,
+                creatorName,
+                null,
+                null,
+                dueDate,
+                initialStockQuantity,
+                stockQuantity
+        );
     }
 }
