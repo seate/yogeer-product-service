3f90f848a1c01a070406cc597fa4100eb81f9080
haebyun <bhk4392@gmail.com>
2024-10-09 17:35:15 +0900

    refactor: 필요없는 priceByQuantities 필드 삭제

--- src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleDemoProductResponseDTO.java
+++ src/main/java/com/yoger/productserviceorganization/product/dto/response/SimpleDemoProductResponseDTO.java
@@ -1,14 +1,11 @@
 package com.yoger.productserviceorganization.product.dto.response;
 
-import com.yoger.productserviceorganization.product.domain.model.PriceByQuantity;
 import com.yoger.productserviceorganization.product.domain.model.ProductState;
 import com.yoger.productserviceorganization.product.persistence.ProductEntity;
-import java.util.List;
 
 public record SimpleDemoProductResponseDTO(
         Long id,
         String name,
-        List<PriceByQuantity> priceByQuantities,
         String thumbnailImageUrl,
         String creatorName,
         ProductState state
@@ -17,7 +14,6 @@ public record SimpleDemoProductResponseDTO(
         return new SimpleDemoProductResponseDTO(
                 productEntity.getId(),
                 productEntity.getName(),
-                productEntity.getPriceByQuantities(),
                 productEntity.getThumbnailImageUrl(),
                 productEntity.getCreatorName(),
                 productEntity.getState()
