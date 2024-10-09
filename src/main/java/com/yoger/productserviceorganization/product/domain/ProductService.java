891919e3efc171ce53c20d243ebcdcc99299a4fd
haebyun <bhk4392@gmail.com>
2024-10-09 15:45:08 +0900

    feat: ProductService에 @Transactional 어노테이션 추가
    - 읽기 전용 트랜잭션 최적화를 위해 viewSellableProducts, viewDemoProducts 메서드에 @Transactional(readOnly = true) 추가
    - 데이터 변경이 발생하는 saveDemoProduct 메서드에 @Transactional 추가
    - 데이터베이스 접근 성능을 향상시키기 위해 트랜잭션 관리 개선

--- src/main/java/com/yoger/productserviceorganization/product/domain/ProductService.java
+++ src/main/java/com/yoger/productserviceorganization/product/domain/ProductService.java
@@ -12,9 +12,11 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
     private final S3Service s3Service;
@@ -29,6 +31,7 @@ public class ProductService {
         return SimpleSellableProductResponseDTO.from(productEntity);
     }
 
+    @Transactional
     public DemoProductResponseDTO saveDemoProduct(@Valid DemoProductRequestDTO demoProductRequestDTO) {
         String imageUrl = s3Service.uploadImage(demoProductRequestDTO.image());
         String thumbnailImageUrl = s3Service.uploadImage(demoProductRequestDTO.thumbnailImage());
