fe0a1ac95a9c8157156fdc9f205dc35b64eb89fc
haebyun <bhk4392@gmail.com>
2024-10-10 15:58:13 +0900

    feat: ProductMapper 클래스 재구현
    - Product와 ProductEntity 간의 상호 변환을 위한 매핑 메서드 추가
      - toEntityFrom(Product): 도메인 엔티티(Product)를 영속성 엔티티(ProductEntity)로 변환
      - toDomainFrom(ProductEntity): 영속성 엔티티(ProductEntity)를 도메인 엔티티(Product)로 변환
    - DemoProductRequestDTO를 기반으로 도메인 엔티티(Product) 생성하는 매핑 메서드 추가

--- src/main/java/com/yoger/productserviceorganization/product/mapper/ProductMapper.java
+++ src/main/java/com/yoger/productserviceorganization/product/mapper/ProductMapper.java
@@ -1,21 +1,59 @@
 package com.yoger.productserviceorganization.product.mapper;
 
+import com.yoger.productserviceorganization.product.domain.model.Product;
 import com.yoger.productserviceorganization.product.domain.model.ProductState;
 import com.yoger.productserviceorganization.product.dto.request.DemoProductRequestDTO;
 import com.yoger.productserviceorganization.product.persistence.ProductEntity;
 
 public class ProductMapper {
-    public static ProductEntity toPersistenceFrom(DemoProductRequestDTO productRequestDTO, String imageUrl,
-                                                  String thumbnailImageUrl) {
+    public static ProductEntity toEntityFrom(Product product) {
         return ProductEntity.of(
-                productRequestDTO.name(),
-                null, // 나중에 설정
-                productRequestDTO.description(),
+                product.getName(),
+                product.getPriceByQuantities(),
+                product.getDescription(),
+                product.getImageUrl(),
+                product.getState(),
+                product.getThumbnailImageUrl(),
+                product.getCreatorId(),
+                product.getCreatorName(),
+                product.getDueDate(),
+                product.getInitialStockQuantity(),
+                product.getStockQuantity()
+        );
+    }
+
+    public static Product toDomainFrom(ProductEntity productEntity) {
+        return Product.of(
+                productEntity.getId(),
+                productEntity.getName(),
+                productEntity.getPriceByQuantities(),
+                productEntity.getDescription(),
+                productEntity.getImageUrl(),
+                productEntity.getState(),
+                productEntity.getThumbnailImageUrl(),
+                productEntity.getCreatorId(),
+                productEntity.getCreatorName(),
+                productEntity.getDueDate(),
+                productEntity.getInitialStockQuantity(),
+                productEntity.getStockQuantity()
+        );
+    }
+
+    public static Product toDomainFrom(
+            DemoProductRequestDTO demoProductRequestDTO,
+            String imageUrl,
+            String thumbnailImageUrl
+    ) {
+        return Product.of(
+                null, // ID는 아직 생성되지 않았으므로 null
+                demoProductRequestDTO.name(),
+                null,
+                demoProductRequestDTO.description(),
                 imageUrl,
                 ProductState.DEMO,
                 thumbnailImageUrl,
-                productRequestDTO.creatorId(),
-                productRequestDTO.creatorName(),
+                demoProductRequestDTO.creatorId(),
+                demoProductRequestDTO.creatorName(),
                 null,
                 0,
                 0
