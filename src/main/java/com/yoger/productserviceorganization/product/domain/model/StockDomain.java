1fe177f1c2029fa8568ec253fa68a36fd5c777cd
haebyun <bhk4392@gmail.com>
2024-10-10 15:52:21 +0900

    refactor: StockDomain 클래스의 예외 처리 변경
    - IllegalArgumentException과 IllegalStateException을 커스텀 예외 (InvalidStockException, InsufficientStockException)로 변경
    - 재고 유효성 검증 및 재고 감소 로직에서 커스텀 예외 사용으로 코드 가독성 및 도메인 일관성 강화

--- src/main/java/com/yoger/productserviceorganization/product/domain/model/StockDomain.java
+++ src/main/java/com/yoger/productserviceorganization/product/domain/model/StockDomain.java
@@ -1,18 +1,21 @@
 package com.yoger.productserviceorganization.product.domain.model;
 
+import com.yoger.productserviceorganization.product.domain.exception.InsufficientStockException;
+import com.yoger.productserviceorganization.product.domain.exception.InvalidStockException;
+
 public class StockDomain {
     private final int initialStockQuantity;
     private int stockQuantity;
 
     StockDomain(int initialStockQuantity, int stockQuantity) {
         if (initialStockQuantity < 0) {
-            throw new IllegalArgumentException("초기 재고 수량은 0보다 작을 수 없습니다.");
+            throw new InvalidStockException("초기 재고 수량은 0보다 작을 수 없습니다.");
         }
         if (stockQuantity < 0) {
-            throw new IllegalArgumentException("현재 재고 수량은 0보다 작을 수 없습니다.");
+            throw new InvalidStockException("현재 재고 수량은 0보다 작을 수 없습니다.");
         }
         if (initialStockQuantity < stockQuantity) {
-            throw new IllegalArgumentException("현재 재고가 초기 재고 수량보다 많을 수 없습니다.");
+            throw new InvalidStockException("현재 재고가 초기 재고 수량보다 많을 수 없습니다.");
         }
         this.initialStockQuantity = initialStockQuantity;
         this.stockQuantity = stockQuantity;
@@ -20,10 +23,10 @@ public class StockDomain {
 
     void decrease(int amount) {
         if (amount <= 0) {
-            throw new IllegalArgumentException("감소할 수량은 0보다 커야 합니다.");
+            throw new InvalidStockException("감소할 수량은 0보다 커야 합니다.");
         }
         if (this.stockQuantity < amount) {
-            throw new IllegalStateException("재고 수량이 부족합니다.");
+            throw new InsufficientStockException("재고 수량이 부족합니다.");
         }
         this.stockQuantity-=amount;
     }
