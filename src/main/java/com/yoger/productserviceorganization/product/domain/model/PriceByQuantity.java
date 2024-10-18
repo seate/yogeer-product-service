dd5ca2f5a266c7565e730aae353b259a6cc9c522
haebyun <bhk4392@gmail.com>
2024-10-18 14:26:20 +0900

    docs(PriceByQuantity): 이후 해결해야할 문제 주석 추가

--- src/main/java/com/yoger/productserviceorganization/product/domain/model/PriceByQuantity.java
+++ src/main/java/com/yoger/productserviceorganization/product/domain/model/PriceByQuantity.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record PriceByQuantity(
+        //TODO: jakarta와 관련된 어노테이션을 추후 모두 삭제 해야함..
+        // 도메인 계층은 런타임에 작동하는 외부 프레임워크와 의존관계가 존재하면 안 됨(헥사고날 아키텍처).
         @NotNull(message = "수량에 대한 정보를 입력해주세요.")
         @Min(value = 0, message = "수량은 0 이상이어야 합니다.")
         int quantity,
