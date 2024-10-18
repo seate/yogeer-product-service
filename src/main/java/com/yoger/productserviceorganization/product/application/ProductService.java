b4008aa2de8f8812d294f76c50f6256efc64e38b
haebyun <bhk4392@gmail.com>
2024-10-18 14:40:59 +0900

    refactor(ProductService) 인터페이스와 구현체로 분리
    - ProductService 인터페이스 추가로 서비스 계층의 유연성 및 확장성 향상
    - 비즈니스 로직은 ProductServiceImpl에서 구현하도록 구조 변경

--- src/main/java/com/yoger/productserviceorganization/product/application/ProductService.java
+++ src/main/java/com/yoger/productserviceorganization/product/application/ProductService.java
@@ -1,95 +1,19 @@
 package com.yoger.productserviceorganization.product.application;
 
-import com.yoger.productserviceorganization.product.adapters.web.dto.request.UpdatedDemoProductRequestDTO;
-import com.yoger.productserviceorganization.product.domain.port.ProductRepository;
-import com.yoger.productserviceorganization.product.domain.model.Product;
-import com.yoger.productserviceorganization.product.domain.model.ProductState;
-import com.yoger.productserviceorganization.product.domain.port.ImageStorageService;
 import com.yoger.productserviceorganization.product.adapters.web.dto.request.DemoProductRequestDTO;
+import com.yoger.productserviceorganization.product.adapters.web.dto.request.UpdatedDemoProductRequestDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.DemoProductResponseDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.SellableProductResponseDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.SimpleDemoProductResponseDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.SimpleSellableProductResponseDTO;
-import com.yoger.productserviceorganization.product.mapper.ProductMapper;
 import jakarta.validation.Valid;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class ProductService {
-    private final ProductRepository productRepository;
-    private final ImageStorageService imageStorageService;
-
-    public List<SimpleSellableProductResponseDTO> findSimpleSellableProducts() {
-        return productRepository.findByState(ProductState.SELLABLE).stream()
-                .map(this::mapToSellableDTO)
-                .toList();
-    }
-
-    private SimpleSellableProductResponseDTO mapToSellableDTO(Product product) {
-        return SimpleSellableProductResponseDTO.from(product);
-    }
-
-    @Transactional
-    public DemoProductResponseDTO saveDemoProduct(@Valid DemoProductRequestDTO demoProductRequestDTO) {
-        String imageUrl = imageStorageService.uploadImage(demoProductRequestDTO.image());
-        String thumbnailImageUrl = imageStorageService.uploadImage(demoProductRequestDTO.thumbnailImage());
-        Product product = ProductMapper.toDomainFrom(demoProductRequestDTO, imageUrl, thumbnailImageUrl);
-        return DemoProductResponseDTO.from(productRepository.save(product));
-    }
-
-    public List<SimpleDemoProductResponseDTO> findSimpleDemoProducts() {
-        return productRepository.findByState(ProductState.DEMO).stream()
-                .map(this::mapToDemoDTO)
-                .toList();
-    }
-
-    private SimpleDemoProductResponseDTO mapToDemoDTO(Product product) {
-        return SimpleDemoProductResponseDTO.from(product);
-    }
-
-    public SellableProductResponseDTO findSellableProduct(Long productId) {
-        Product product = productRepository.findById(productId);
-        product.validateUnexpectedState(ProductState.SELLABLE);
-        return SellableProductResponseDTO.from(product);
-    }
-
-    public DemoProductResponseDTO findDemoProduct(Long productId) {
-        Product product = productRepository.findById(productId);
-        product.validateUnexpectedState(ProductState.DEMO);
-        return DemoProductResponseDTO.from(product);
-    }
-
-    @Transactional
-    public DemoProductResponseDTO updateDemoProduct(Long productId,
-                                                    UpdatedDemoProductRequestDTO updatedDemoProductRequestDTO) {
-        Product product = productRepository.findById(productId);
-        product.validateUnexpectedState(ProductState.DEMO);
-
-        if (updatedDemoProductRequestDTO.image() != null && !updatedDemoProductRequestDTO.image().isEmpty()) {
-            String newImageUrl = imageStorageService.uploadImage(updatedDemoProductRequestDTO.image());
-            imageStorageService.deleteImage(product.getImageUrl());
-            product.updateImageUrl(newImageUrl);
-        }
-
-        if (updatedDemoProductRequestDTO.thumbnailImage() != null && !updatedDemoProductRequestDTO.thumbnailImage().isEmpty()) {
-            String newThumbnailImageUrl = imageStorageService.uploadImage(updatedDemoProductRequestDTO.thumbnailImage());
-            imageStorageService.deleteImage(product.getThumbnailImageUrl());
-            product.updateThumbnailImageUrl(newThumbnailImageUrl);
-        }
-
-        if (updatedDemoProductRequestDTO.name() != null) {
-            product.updateName(updatedDemoProductRequestDTO.name());
-        }
-
-        if (updatedDemoProductRequestDTO.description() != null) {
-            product.updateDescription(updatedDemoProductRequestDTO.description());
-        }
-
-        return DemoProductResponseDTO.from(product);
-    }
+public interface ProductService {
+    List<SimpleSellableProductResponseDTO> findSimpleSellableProducts();
+    DemoProductResponseDTO saveDemoProduct(@Valid DemoProductRequestDTO demoProductRequestDTO);
+    List<SimpleDemoProductResponseDTO> findSimpleDemoProducts();
+    SellableProductResponseDTO findSellableProduct(Long productId);
+    DemoProductResponseDTO findDemoProduct(Long productId);
+    DemoProductResponseDTO updateDemoProduct(Long productId, UpdatedDemoProductRequestDTO updatedDemoProductRequestDTO);
 }
