70adfec2307b596caf6ea47bc3ed8b2936e7b66c
haebyun <bhk4392@gmail.com>
2024-11-16 16:59:15 +0900

    feat(SellableProductController): 재고 변경 요청을 받는 엔드포인트 추가

--- src/main/java/com/yoger/productserviceorganization/product/adapters/web/controller/SellableProductController.java
+++ src/main/java/com/yoger/productserviceorganization/product/adapters/web/controller/SellableProductController.java
@@ -1,13 +1,17 @@
 package com.yoger.productserviceorganization.product.adapters.web.controller;
 
+import com.yoger.productserviceorganization.product.adapters.web.dto.request.StockChangeRequestDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.SellableProductResponseDTO;
 import com.yoger.productserviceorganization.product.adapters.web.dto.response.SimpleSellableProductResponseDTO;
 import com.yoger.productserviceorganization.product.application.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +31,14 @@ public class SellableProductController {
     public ResponseEntity<SellableProductResponseDTO> getSellableProduct(@PathVariable Long productId) {
         SellableProductResponseDTO sellableProductDTO = productService.findSellableProduct(productId);
         return ResponseEntity.ok(sellableProductDTO);
+    }
+
+    @PatchMapping("/{productId}/stock/change")
+    public ResponseEntity<Void> changeStock(
+            @PathVariable Long productId,
+            @RequestBody StockChangeRequestDTO stockChangeRequestDTO
+    ) {
+        productService.changeSellableProductStock(productId, stockChangeRequestDTO.quantity());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
