25be2b20fb59ac8d570677c33e6581c6b24c4d34
haebyun <bhk4392@gmail.com>
2024-12-01 21:30:17 +0900

    feat(RedisCacheConfig): Redis 캐시 설정 추가
    - `@EnableCaching` 어노테이션을 통해 캐싱 기능 활성화
    - `RedisConnectionFactory` 빈을 정의하여 Redis 서버와의 연결 설정
    - `RedisTemplate` 빈을 설정하고, 키 직렬화기를 `StringRedisSerializer`로 지정
    - `Jackson2JsonRedisSerializer`를 사용하여 값 직렬화기를 JSON 형식으로 설정
    - `ObjectMapper`에 `JavaTimeModule` 등록 및 기본 타이핑 활성화

---
+++ src/main/java/com/yoger/productserviceorganization/product/config/RedisCacheConfig.java
@@ -0,0 +1,39 @@
+package com.yoger.productserviceorganization.product.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("127.0.0.1", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+
+        redisTemplate.setValueSerializer(serializer);
+        return redisTemplate;
+    }
+}
