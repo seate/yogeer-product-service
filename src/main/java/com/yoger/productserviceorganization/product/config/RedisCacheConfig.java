99e2ecd96104b7bc3fef3c111e0ea2921efd20a5
haebyun <bhk4392@gmail.com>
2024-12-08 21:54:27 +0900

    feat(RedisCacheConfig): password도 받을 수 있도록 기능 구현

--- src/main/java/com/yoger/productserviceorganization/product/config/RedisCacheConfig.java
+++ src/main/java/com/yoger/productserviceorganization/product/config/RedisCacheConfig.java
@@ -10,6 +10,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -22,10 +24,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisCacheConfig {
     private String host;
     private Integer port;
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        if (password != null && !password.isEmpty()) {
+            config.setPassword(RedisPassword.of(password));
+        }
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
